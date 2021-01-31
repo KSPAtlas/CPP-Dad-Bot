a199508f12ee62a0f550ba7cd5d3d56cae627cd5
TheGamingCoderNoob <72895702+TheGamingCoderNoob@users.noreply.github.com>
2021-01-31 21:12:08 +0000

    Fixed up code

--- Uncompiled.cpp
+++ Uncompiled.cpp
@@ -1,14 +1,21 @@
+// Includes
 #include<iostream>
 #include<string>
 #include<algorithm>
+
+// Setting up variables
 std::string findthis = "i\'m";
 std::string input;
 std::string falseinput;
 std::string name;
 bool IsWindows;
-int index;
+int indexvar;
 std::string sub = "Hi";
+
+// Start of "main" function
 int main() {
+
+// Check to see if the user is on Windows
 #ifdef _WIN32
 	std::cout << "You are running in Windows mode.\n";
 	IsWindows = true;
@@ -16,6 +23,8 @@ int main() {
 	std::cout << "You are running in Unix mode.\n";
 	IsWindows = false;
 #endif
+
+    // Asking for name
 	std::cout << "What name would you like me to call you? ";
 	std::getline(std::cin, name);
 	if (IsWindows == true) {
@@ -24,23 +33,30 @@ int main() {
 	else {
 		system("clear");
 	}
+
+    // Start of ask loop
 	while (true) {
+        // Gets user input and makes it lowercase
+        std::cout << "[" << name << "]: ";
 		std::getline(std::cin, input);
 		falseinput = input;
 		std::for_each(falseinput.begin(), falseinput.end(), [](char& c) {
 			c = ::tolower(c);
 			});
-		if ((index = falseinput.find("d!dadjoke")) != std::string::npos) {
-			std::cout << "Oh no! Dad jokes are currently not available. Check back later.\n";
+        // If user asks for dad joke (d!dadjoke)
+		if ((indexvar = falseinput.find("d!dadjoke")) != std::string::npos) {
+			std::cout << "[Dad-Bot]: Oh no! Dad jokes are currently not available. Check back later.\n";
 		}
-		if ((index = falseinput.find("shut up")) != std::string::npos) {
-			std::cout << "Listen here " << name << ", I will not tolerate you saying the words that consist of the letters \'s h u t  u p\' being said in this server, so take your own advice and close thine mouth in the name of the christian minecraft server owner.\n";
+        // If user says "shut up"
+		if ((indexvar = falseinput.find("shut up")) != std::string::npos) {
+			std::cout << "[Dad-Bot]: Listen here " << name << ", I will not tolerate you saying the words that consist of the letters \'s h u t  u p\' being said in this server, so take your own advice and close thine mouth in the name of the christian minecraft server owner.\n";
 		}
-		if ((index = falseinput.find(findthis)) != std::string::npos) {
-			input.replace(index, findthis.length(), sub);
+        // If user says "I'm'
+		if ((indexvar = falseinput.find(findthis)) != std::string::npos) {
+			input.replace(indexvar, findthis.length(), sub);
 			size_t pos = input.find("Hi");
 			input.erase(0, pos);
-			std::cout << input << ", I'm dad!\n";
+			std::cout << "[Dad-Bot]: " << input << ", I'm dad!\n";
 		}
 	}
 }
